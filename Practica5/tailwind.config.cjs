47705cb25f17ffa67432cd2bdfdba60a71c96585
GabrielFranciscoSM <gabrielfranciscosm@gmail.com>
2025-11-27 12:20:54 +0100

    feat: cambio de bootstrap a tailwind

---
+++ Practica5/tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './views/**/*.html',
+    './public/src/**/*.css'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'mercadona-green': '#2d7a3e',
+        'mercadona-orange': '#ff6b00'
+      }
+    }
+  },
+  plugins: [],
+}
